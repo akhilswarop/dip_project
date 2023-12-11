bb955c1ce95fc96bae871340f272f93562cde4c9
Akhil Swarop S <96727867+akhilswarop@users.noreply.github.com>
2023-12-11 10:53:35 +0530

    Added Dilation

---
+++ morhpological_transformation/dilation.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('Test Images/test_1.jpg', cv2.IMREAD_GRAYSCALE)
+kernel_size = 5
+kernel = np.ones((kernel_size, kernel_size), np.uint8)
+dilation_result = cv2.dilate(image, kernel, iterations=1)
+
+cv2.imshow('Original', image)
+cv2.imshow('Dilation', dilation_result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
